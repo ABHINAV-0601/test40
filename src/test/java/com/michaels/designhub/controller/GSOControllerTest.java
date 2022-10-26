af842b338a4c2b7a65e515cfd44893639f5d9200
David Hong <baojian@michaels.com>
2022-10-26 11:31:23 +0800

    Add the Test

--- src/test/java/com/michaels/designhub/controller/GSOControllerTest.java
+++ src/test/java/com/michaels/designhub/controller/GSOControllerTest.java
@@ -1,33 +1,46 @@
 package com.michaels.designhub.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.michaels.designhub.ColorPickerApiApplication;;
 import com.michaels.designhub.request.SearchGSOAndLayoutOptimizationRequest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.michaels.designhub.response.SearchGSOAndLayoutOptimizationResponse;
+import com.michaels.designhub.service.impl.GSOServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
 
 import java.sql.SQLException;
+
+import static org.mockito.Mockito.*;
+
 /**
  * @Author Baojian Hong
- * @Date 2022/9/26 14:11
+ * @Date 2022/10/26 11:27
  * @Version 1.0
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = ColorPickerApiApplication.class)
-public class GSOControllerTest {
+class GSOControllerTest {
+    @Mock
+    Logger logger;
+    @Mock
+    GSOServiceImpl gsoService;
+    @InjectMocks
+    GSOController gSOController;
 
-    @Autowired
-    private GSOController gsoController;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
-    public void utilsGso() throws SQLException, JsonProcessingException {
-        String json = "{\"store_id\":\"1054\",\"locale\":\"en-us\",\"order_lineitem_ids\":[1187192,1189014,1187320,1198492,1197701,1198493,1205862,1205948,1201086,1206016,1211280,1212745]}";
-        JSONObject jsonObject = JSONObject.parseObject(json);
-        SearchGSOAndLayoutOptimizationRequest searchGSOAndLayoutOptimizationRequest = JSONObject.toJavaObject(jsonObject, SearchGSOAndLayoutOptimizationRequest.class);
-        gsoController.utilsGso(searchGSOAndLayoutOptimizationRequest);
+    void testUtilsGso() throws SQLException, JsonProcessingException {
+        when(gsoService.utilsGso(any())).thenReturn(new SearchGSOAndLayoutOptimizationResponse());
+
+        SearchGSOAndLayoutOptimizationResponse result = gSOController.utilsGso(new SearchGSOAndLayoutOptimizationRequest());
+        Assertions.assertEquals(new SearchGSOAndLayoutOptimizationResponse(), result);
     }
 }
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
