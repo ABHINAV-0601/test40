8851e588f6c6d861df4e058a6708fa613ce76280
Qiang Hu <qiang4@michaels.com>
2022-11-16 10:46:35 +0800

    DH-4-customer-experience-api update the interface response

--- src/main/java/com/michaels/designhub/controller/ColorPickerController.java
+++ src/main/java/com/michaels/designhub/controller/ColorPickerController.java
@@ -1,11 +1,18 @@
 package com.michaels.designhub.controller;
+
+import com.michaels.designhub.entity.ColorPicker;
 import com.michaels.designhub.service.IColorPickerService;
-import com.michaels.designhub.common.dto.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -20,7 +27,23 @@ public class ColorPickerController {
 
     @ResponseBody
     @GetMapping("/colors")
-    public Result<Object> getColor() {
-        return Result.success(colorPickerService.findColorPickerByLocale());
+    public Map<String,Object> getColor(String locale) {
+        List<ColorPicker> list = colorPickerService.findColorPickerByLocale();
+        Map<String,Object> map = new HashMap<>();
+        List<Map<String,Object>> colorList = new ArrayList<>();
+        for(ColorPicker cp:list){
+            Map<String,Object> color = new HashMap<>();
+            color.put("id",cp.getId());
+            color.put("value",cp.getValue());
+            color.put("rgb_value",cp.getRbgValue());
+            if(StringUtils.hasLength(locale) && locale.equals("en_US")){
+                color.put("name",cp.getNameEn());
+            }else{
+                color.put("name",cp.getNameFr());
+            }
+            colorList.add(color);
+        }
+        map.put("color_family",colorList);
+        return map;
     }
 }
