2d0276bddcfeac3d7d91a7c8b4d13e4340be467e
Qiang Hu <qiang4@michaels.com>
2022-11-17 16:48:09 +0800

    DH-4-customer-experience-api test

--- src/main/java/com/michaels/designhub/controller/GSOController.java
+++ src/main/java/com/michaels/designhub/controller/GSOController.java
@@ -1,20 +1,16 @@
 package com.michaels.designhub.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.michaels.designhub.dto.UtilsDto;
 import com.michaels.designhub.request.SearchGSOAndLayoutOptimizationRequest;
 import com.michaels.designhub.response.SearchGSOAndLayoutOptimizationResponse;
 import com.michaels.designhub.service.impl.GSOServiceImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
 import java.util.Map;
 
 /**
@@ -31,7 +27,7 @@ public class GSOController {
 
     @PostMapping("/utils/gso")
     public SearchGSOAndLayoutOptimizationResponse utilsGso(@RequestBody SearchGSOAndLayoutOptimizationRequest searchGSOAndLayoutOptimizationRequest) throws Exception {
-        log.info("In Search GSO and Optimization flow");
+        log.info("In Search GSO and Optimization flow.");
         log.info(searchGSOAndLayoutOptimizationRequest.toString());
         SearchGSOAndLayoutOptimizationResponse searchGSOAndLayoutOptimizationResponse = gsoService.utilsGso(searchGSOAndLayoutOptimizationRequest);
         return searchGSOAndLayoutOptimizationResponse;
@@ -39,7 +35,7 @@ public class GSOController {
 
     @PostMapping("/utils")
     public Map<String,Object> utils(@RequestBody UtilsDto utilsDto) {
-        log.info("In Search GSO and Optimization flow");
+        log.info("In Search GSO and Optimization flow.");
         Map<String,Object> result = gsoService.utils(utilsDto);
         return result;
     }
