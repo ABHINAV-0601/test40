f75a80e7aa57cc01bc8fbb957566a9f77d89a693
Abdul Ashik Khan <abdulash@michaels.com>
2023-02-08 15:15:22 -0600

    DH-248 | Removed training_log persist as it is handled by dhapi

--- src/main/java/com/michaels/designhub/controller/UtilsController.java
+++ src/main/java/com/michaels/designhub/controller/UtilsController.java
@@ -1,10 +1,8 @@
 package com.michaels.designhub.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.michaels.designhub.dto.UtilsDto;
 import com.michaels.designhub.request.SearchGSOAndLayoutOptimizationRequest;
 import com.michaels.designhub.response.SearchGSOAndLayoutOptimizationResponse;
-import com.michaels.designhub.service.TrainingService;
 import com.michaels.designhub.service.impl.UtilsServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +22,6 @@ public class UtilsController {
     @Autowired
     private UtilsServiceImpl utilsService;
 
-    @Autowired
-    private TrainingService trainingService;
-
     @PostMapping("/utils/gso")
     public SearchGSOAndLayoutOptimizationResponse utilsGso(@RequestBody SearchGSOAndLayoutOptimizationRequest searchGSOAndLayoutOptimizationRequest) throws Exception {
         log.info("In Search GSO and Optimization flow");
@@ -40,23 +35,4 @@ public class UtilsController {
         return utilsService.utils(utilsDto);
     }
 
-    /** If a user selects training mode in UI,
-     *  if checkbox is enabled, [method : saveTrainingLog]
-     *      request     : firstname, lastname, role and store id.
-     *      response    : id
-     *  else If checkbox is disabled, [method : updateTrainingLog}
-     *      request     : id
-     *      response    : success/failure
-     */
-
-    @ResponseBody
-    @PostMapping("/utils/training")
-    public Object trainingPOST(@RequestBody JSONObject trainingObject) {
-        return trainingService.saveTrainingLog(trainingObject);
-    }
-
-    @PutMapping("/utils/training")
-    public void trainingPUT(@RequestBody JSONObject trainingPutObject) {
-        trainingService.updateTrainingLog(trainingPutObject);
-    }
 }
