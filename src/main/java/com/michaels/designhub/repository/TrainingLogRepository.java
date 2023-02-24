54e674faef3e63009069c6e1ffe5d5f93f7bb53c
VENKATRE <venkatre@michaels.com>
2023-02-24 10:48:04 -0600

    catalogs end-point changes

--- src/main/java/com/michaels/designhub/repository/TrainingLogRepository.java
+++ src/main/java/com/michaels/designhub/repository/TrainingLogRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UtilRepository extends JpaRepository<TrainingLog, Integer> {
+public interface TrainingLogRepository extends JpaRepository<TrainingLog, Integer> {
 
 }
