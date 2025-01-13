b04aa098ed0a52c6d375d7f858ee0c284c084a29
lankesh1 <lankesh@michaels.com>
2025-01-13 20:36:49 +0530

    Encode Dto

--- src/main/java/com/michaels/designhub/controller/UtilsController.java
+++ src/main/java/com/michaels/designhub/controller/UtilsController.java
@@ -8,10 +8,12 @@ import com.michaels.designhub.response.SearchGSOAndLayoutOptimizationResponse;
 import com.michaels.designhub.response.TrackingNumberResponse;
 import com.michaels.designhub.service.impl.UtilsServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.owasp.encoder.Encode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,8 +38,7 @@ public class UtilsController {
 
     @PostMapping("/utils")
     public Map<String, Object> utils(@RequestBody UtilsDto utilsDto, HttpServletRequest httpServletRequest) {
-        log.debug("In Utils and calling function  and function params - {}", utilsDto);
-        return utilsService.utils(utilsDto, httpServletRequest);
+        return utilsService.utils(encodeDto(utilsDto), httpServletRequest);
     }
 
     @PostMapping("/utils/training")
@@ -52,12 +53,39 @@ public class UtilsController {
 
     @PostMapping("/utils/tracking-numbers")
     public TrackingNumberResponse trackingNumbers(@RequestBody TrackingNumberDto trackingNumberDto) {
-        return utilsService.updateTrackingNumbers(trackingNumberDto);
+        return utilsService.updateTrackingNumbers(encodeDto(trackingNumberDto));
 
     }
 
     @PostMapping("/utils/tracking-status-update")
     public TrackingNumberResponse trackingNumbersStatusUpdate(@RequestBody TrackingNumberDto trackingNumberDto) {
-        return utilsService.updateComponentStatusForTrackingNumbers(trackingNumberDto);
+        return utilsService.updateComponentStatusForTrackingNumbers(encodeDto(trackingNumberDto));
+    }
+
+    // Generic method to encode HTML for all String fields in a DTO
+    public static <T> T encodeDto(T dto) {
+        // Get all fields of the DTO class
+        Field[] fields = dto.getClass().getDeclaredFields();
+
+        // Iterate over all fields
+        for (Field field : fields) {
+            // Make the field accessible if it's private
+            field.setAccessible(true);
+
+            try {
+                // Check if the field is of type String
+                if (field.get(dto) instanceof String) {
+                    String value = (String) field.get(dto);
+                    // Encode the String value for HTML
+                    String encodedValue = Encode.forHtml(value);
+                    // Set the encoded value back to the field
+                    field.set(dto, encodedValue);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return dto;
     }
 }
